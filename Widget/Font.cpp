1a2aedd66e23f0a63fb6c43f0cf877491a4d70b3
Zsolt Tóth <waysofdarkness@gmail.com>
2022-05-27 21:30:21 +0200

    Font rendering fixed.

--- Widget/Font.cpp
+++ Widget/Font.cpp
@@ -21,12 +21,13 @@ Font::Font(FT_Face fontface)
 void Font::renderText(GuiRenderer& renderer, const std::string& text, const glm::fvec2& offset, float scale, const glm::fvec4& color)
 {
 	float x = offset.x;
-	float y = offset.x;
+	float y = offset.y;
 	for(const auto c : text) {
 		auto charIt = characters.find(c);
 		if(charIt == std::end(characters)) continue;
-		const glm::fvec2 pos = glm::fvec2(x + charIt->second.bearing.x * scale,y + (charIt->second.size.y - charIt->second.bearing.y) * scale);
-		const glm::fvec2 dim = glm::fvec2(charIt->second.size.x * scale,charIt->second.size.y * scale);
+		const glm::fvec2 pos = glm::fvec2(x + (charIt->second.bearing.x * scale),
+										  y + ((charIt->second.size.y - charIt->second.bearing.y) * scale));
+		const glm::fvec2 dim = glm::fvec2(charIt->second.size.x * scale,charIt->second.size.y * -scale);
 		renderer.renderTex( pos, pos + dim, charIt->second.texture );
 		x += (charIt->second.advance >> 6) * scale;
 	}
